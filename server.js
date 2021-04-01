183eddf85a14d02a56278ed3a15c0b99a8b3db89
alcalaluis <alcalaluis@rocketmail.com>
2021-03-31 19:42:58 -0500

    server.js set up

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const logger = require("morgan")
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
